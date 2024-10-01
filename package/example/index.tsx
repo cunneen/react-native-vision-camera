ed8a72455500a263aa59aea71d793c6bfdeeec5b
Mike Cunneen <mike@appworkshop.net>
2024-10-01 12:52:29 +0800

    refactor only : move example app to "visioncamera" subfolder

--- package/example/index.tsx
+++ package/example/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import 'react-native-gesture-handler'
 import { AppRegistry, Button, StyleSheet, View } from 'react-native'
-import { VisionCamera } from './src/VisionCamera'
+import { VisionCamera } from './src/visioncamera/VisionCamera'
 import { initialWindowMetrics } from 'react-native-safe-area-context'
 
+/**
+ * This is an example of using React Native Vision Camera as a module within
+ * a larger app.
+ * The "VisionCamera" component is a simplified version of the example app from
+ * the VisionCamera repo: https://github.com/mrousavy/react-native-vision-camera/tree/main/package/example
+ */
 function VisionCameraExample(): JSX.Element {
   const [shouldShow, setShouldShow] = React.useState(false)
 
